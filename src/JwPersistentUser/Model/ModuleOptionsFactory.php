57356b606ac157312de5902793e952a3eaa27604
Roel van Duijnhoven <roel.duijnhoven@gmail.com>
2014-06-06 09:28:01 +0200

    Make entitty class configurable via ModuleOptions.

--- src/JwPersistentUser/Model/ModuleOptionsFactory.php
+++ src/JwPersistentUser/Model/ModuleOptionsFactory.php
@@ -7,8 +7,17 @@ use Zend\ServiceManager\FactoryInterface,
 
 class ModuleOptionsFactory implements FactoryInterface
 {
+    const KEY = 'jwpersistentuser';
+
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        return new ModuleOptions;
+        $options = new ModuleOptions;
+
+        $config = $serviceLocator->get('Config');
+        if (isset($config[self::KEY]) && is_array($config[self::KEY])) {
+            $options->setFromArray($config[self::KEY]);
+        }
+
+        return $options;
     }
 }
